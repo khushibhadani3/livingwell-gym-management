5b0d6c98ca556f7c7ef7bfadb554254cb4bcfabe
Khushi Bhadani <khushippatel003@gmail.com>
2024-10-24 02:46:55 +0530

    user profile added

--- user_profile.php
+++ user_profile.php
@@ -59,7 +59,7 @@ $conn->close();
     <meta name="keywords" content="Gym, unica, creative, html">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>livingWell</title>
+    <title>User Profile</title>
 
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css?family=Muli:300,400,500,600,700,800,900&display=swap" rel="stylesheet">
